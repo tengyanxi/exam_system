c97b985e8e646b4e5b52d3dcc1fd8e0c0d957c88
Ninglo <1871391898@qq.com>
2019-11-17 15:18:54 +0800

    重构了获得题库的函数

---
+++ question_bank/get_path.py
@@ -0,0 +1,28 @@
+import os
+
+def find_bank(bank_name):
+    path = os.getcwd()
+    print(path)
+    dir_list = os.listdir(path)
+    print(dir_list)
+    print(bank_name)
+    for bank_dir in dir_list:
+        if bank_dir.find(bank_name) != -1:  # -1代表文件夹名称中没有bank_name
+            bank_path = path + "/" + bank_dir
+            print(bank_path)
+            return bank_path
+
+def find_question(bank_path, type_, chapters):
+    file_list = os.listdir(bank_path)
+    whole_question_list = []
+    for file in file_list:
+        if file.find(type_) != -1:
+            file_path = bank_path + "/" + file
+            whole_question_list.append(file_path)
+    question_list = []
+    for file in whole_question_list:
+        for chapter in chapters:
+            if file.find(chapter) != -1:
+                question_list.append(file)
+                break
+    return question_list
