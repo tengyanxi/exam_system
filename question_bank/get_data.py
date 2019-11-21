75470b17ed16b8345154c84139d5b9b7d768c4ab
Ninglo <1871391898@qq.com>
2019-11-21 22:43:22 +0800

    将题库转为json文件, 提升读取效率

---
+++ question_bank/get_data.py
@@ -0,0 +1,20 @@
+import json
+
+def get_data(index):
+    """
+    输入数据的ID的任意前N位
+    以list形式返回符合该ID的任意前N位的所有数据
+    """
+    with open('data.json', 'r', encoding='utf-8') as f:
+        file_data = json.load(f)
+        data_list = []
+        num = len(index)
+        for key, _ in file_data.items():
+            if key[:num].find(index):
+                data = file_data[key]
+                data_list.append(data)
+        return data_list
+
+
+if __name__ == "__main__":
+    print(get_data('0201')[-1])
